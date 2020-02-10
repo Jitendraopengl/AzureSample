56f6cbf3060e676cf835380f11e35b78b23fd28b
Jitendraopengl <60812783+Jitendraopengl@users.noreply.github.com>
2020-02-10 23:57:56 +0530

    + Azure Queuq

---
+++ AddMsgInAzQueue/AddMsgInAzQueue/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+
+namespace AddMsgInAzQueue
+{
+    class Program
+    {
+        static QueueClient queueClient;
+        static void Main(string[] args)
+        {
+            string sbConnectionString = "Endpoint=sb://mobilerechargeservicebus.servicebus.windows.net/;SharedAccessKeyName=RechargeAccesspolicy;SharedAccessKey=mxx7P4Pb9UtFt0pqu1kU7etj0gskrzft5fy2szcp6M0=";
+
+            string sbQueueName = "rechargemq";
+            //;EntityPath=rechargemq
+            string messageBody = string.Empty;
+            try
+            {
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("Mobile Recharge");
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("Operators");
+                Console.WriteLine("1. Vodafone");
+                Console.WriteLine("2. Airtel");
+                Console.WriteLine("3. JIO");
+                Console.WriteLine("-------------------------------------------------------");
+
+                Console.WriteLine("Operator:");
+                string mobileOperator = Console.ReadLine();
+                Console.WriteLine("Amount:");
+                string amount = Console.ReadLine();
+                Console.WriteLine("Mobile:");
+                string mobile = Console.ReadLine();
+
+                Console.WriteLine("-------------------------------------------------------");
+
+                switch (mobileOperator)
+                {
+                    case "1":
+                        mobileOperator = "Vodafone";
+                        break;
+                    case "2":
+                        mobileOperator = "Airtel";
+                        break;
+                    case "3":
+                        mobileOperator = "JIO";
+                        break;
+                    default:
+                        break;
+                }
+
+                messageBody = mobileOperator + "*" + mobile + "*" + amount;
+                queueClient = new QueueClient(sbConnectionString, sbQueueName);
+
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                Console.WriteLine($"Message Added in Queue: {messageBody}");
+                queueClient.SendAsync(message);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+                queueClient.CloseAsync();
+            }
+        }
+    }
+}
